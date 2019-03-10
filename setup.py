d3ccfd2aae66ce9ce17e85d07d7838337568c1ab
Pin-Ju Tien <pinju.tien@gmail.com>
2019-03-10 14:50:23 -0400

    update setup

--- setup.py
+++ setup.py
@@ -17,49 +17,23 @@ with codecs.open('README.rst', encoding='utf-8-sig') as f:
     LONG_DESCRIPTION = f.read()
 MAINTAINER = 'Pin-Ju Tien'
 MAINTAINER_EMAIL = 'pinju.tien@gmail.com'
-URL = 'https://github.com/scikit-learn-contrib/mimic'
-LICENSE = 'new BSD'
-DOWNLOAD_URL = 'https://github.com/scikit-learn-contrib/mimic'
+URL = 'https://github.com/pinjutien/mimic'
+# LICENSE = 'new BSD'
+DOWNLOAD_URL = 'https://github.com/pinjutien/mimic'
 VERSION = __version__
-INSTALL_REQUIRES = ['numpy', 'scipy', 'scikit-learn', 'matplotlib']
-CLASSIFIERS = ['Intended Audience :: Science/Research',
-               'Intended Audience :: Developers',
-               'License :: OSI Approved',
-               'Programming Language :: Python',
-               'Topic :: Software Development',
-               'Topic :: Scientific/Engineering',
-               'Operating System :: Microsoft :: Windows',
-               'Operating System :: POSIX',
-               'Operating System :: Unix',
-               'Operating System :: MacOS',
-               'Programming Language :: Python :: 2.7',
-               'Programming Language :: Python :: 3.5',
-               'Programming Language :: Python :: 3.6',
-               'Programming Language :: Python :: 3.7']
-EXTRAS_REQUIRE = {
-    'tests': [
-        'pytest',
-        'pytest-cov'],
-    'docs': [
-        'sphinx',
-        'sphinx-gallery',
-        'sphinx_rtd_theme',
-        'numpydoc',
-        'matplotlib'
-    ]
-}
-
+INSTALL_REQUIRES = ['numpy', 'scikit-learn', 'matplotlib']
 setup(name=DISTNAME,
       maintainer=MAINTAINER,
       maintainer_email=MAINTAINER_EMAIL,
       description=DESCRIPTION,
-      license=LICENSE,
+      # license=LICENSE,
       url=URL,
       version=VERSION,
       download_url=DOWNLOAD_URL,
       long_description=LONG_DESCRIPTION,
-      zip_safe=False,  # the package can run out of an .egg file
-      classifiers=CLASSIFIERS,
-      packages=find_packages(),
+      # zip_safe=False,  # the package can run out of an .egg file
+      # classifiers=CLASSIFIERS,
+      # packages=find_packages(),
       install_requires=INSTALL_REQUIRES,
-      extras_require=EXTRAS_REQUIRE)
+      # extras_require=EXTRAS_REQUIRE
+)
